41a60d89fdef8a0c1571bd6c446e034d6fbe7d86
gabzreal <balbuenagf@gmail.com>
2026-05-22 23:26:21 +1000

    Add DAO skeleton classes for database layer

---
+++ DRS_Enhanced/src/drsenhanced/dao/CoordinationRecordDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package drsenhanced.dao;
+
+/**
+ * CoordinationRecordDAO handles database operations
+ * related to coordination records.
+ *
+ * @author Gabriel Fernandez Balbuena - 12292617
+ */
+public class CoordinationRecordDAO {
+
+}
---
+++ DRS_Enhanced/src/drsenhanced/dao/EvacuationRecordDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package drsenhanced.dao;
+
+/**
+ * EvacuationRecordDAO handles database operations
+ * related to evacuation records.
+ *
+ * @author Gabriel Fernandez Balbuena - 12292617
+ */
+public class EvacuationRecordDAO {
+
+}
