6e3072abf26ae71d640c204e749e4d32db5a700b
gabzreal <balbuenagf@gmail.com>
2026-05-30 15:45:07 +1000

    Fix JavaFX build and GUI launcher

---
+++ DRS_Enhanced/src/drsenhanced/client/MainLauncher.java
@@ -0,0 +1,11 @@
+package drsenhanced.client;
+
+/**
+ * Starts the JavaFX client from a regular Java main class.
+ */
+public class MainLauncher {
+
+    public static void main(String[] args) {
+        DRSClientApp.main(args);
+    }
+}
