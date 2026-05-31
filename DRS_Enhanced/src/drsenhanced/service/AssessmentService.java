b71cab6b8d0a2947de4e175e5f49a74ad4f1482a
Hajera55 <hajerabegum.shaik@cqumail.com>
2026-05-31 11:42:08 +1000

    Create service layer package and service class skeletons
    Added initial service layer structure for Assessment, Priority, Availability, Shelter, Evacuation and Validation services.

---
+++ DRS_Enhanced/src/drsenhanced/service/AssessmentService.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package drsenhanced.service;
+
+/**
+ *
+ * @author Hajera Begum Shaik - 12281382
+ */
+public class AssessmentService {
+    
+   
+    
+}
