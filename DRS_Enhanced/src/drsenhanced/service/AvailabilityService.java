7e75d13a00e8b40e5e0818161ea76992299005c1
Hajera55 <hajerabegum.shaik@cqumail.com>
2026-05-31 12:13:29 +1000

    Implement resource and department availability service
    Added resource availability, department availability and availability percentage calculations.

--- DRS_Enhanced/src/drsenhanced/service/AvailabilityService.java
+++ DRS_Enhanced/src/drsenhanced/service/AvailabilityService.java
@@ -5,10 +5,45 @@
 package drsenhanced.service;
 
 /**
+ * Handles resource and department availability operations.
  *
- * @author Hajera Begum Shaik - 12281382
+ * @author Hajera Begum Shaik
  */
 public class AvailabilityService {
-    
-    
+
+    /**
+     * Checks whether a resource is available.
+     *
+     * @param availableUnits available quantity
+     * @return true if resource is available
+     */
+    public boolean checkResourceAvailability(int availableUnits) {
+        return availableUnits > 0;
     }
+
+    /**
+     * Checks whether a department is operational.
+     *
+     * @param staffAvailable available staff members
+     * @return true if department can operate
+     */
+    public boolean checkDepartmentAvailability(int staffAvailable) {
+        return staffAvailable >= 2;
+    }
+
+    /**
+     * Calculates availability percentage.
+     *
+     * @param available available resources
+     * @param total total resources
+     * @return availability percentage
+     */
+    public double calculateAvailabilityPercentage(int available, int total) {
+
+        if (total <= 0) {
+            return 0;
+        }
+
+        return ((double) available / total) * 100;
+    }
+}
