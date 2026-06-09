b4e455179cbc4ea164fa12731ba0ace340e692aa
nitishrayapati <nitish.rayapati@cqumail.com>
2026-06-10 03:58:00 +1000

    Add user role model classes

---
+++ DRS_Enhanced/src/drsenhanced/model/User.java
@@ -0,0 +1,75 @@
+package drsenhanced.model;
+
+/**
+ * Represents a basic user in the Disaster Response System.
+ * This is the parent class for Citizen, EmergencyWorker and CityManager.
+ */
+public class User {
+
+    private int userId;
+    private String username;
+    private String password;
+    private String role;
+
+    /**
+     * Default constructor.
+     */
+    public User() {
+    }
+
+    /**
+     * Creates a user with login and role details.
+     *
+     * @param userId the user ID
+     * @param username the username
+     * @param password the password
+     * @param role the user role
+     */
+    public User(int userId, String username, String password, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "userId=" + this.userId
+                + ", username='" + this.username + '\''
+                + ", role='" + this.role + '\''
+                + '}';
+    }
+}
