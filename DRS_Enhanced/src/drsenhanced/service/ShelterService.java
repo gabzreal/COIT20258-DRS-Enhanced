0974eccc8190855625621760bd8c3d8b2de97c2f
Hajera55 <hajerabegum.shaik@cqumail.com>
2026-06-11 02:48:54 +1000

    Implement ShelterService using ShelterDAO

--- DRS_Enhanced/src/drsenhanced/service/ShelterService.java
+++ DRS_Enhanced/src/drsenhanced/service/ShelterService.java
@@ -19,47 +19,20 @@ public class ShelterService {
 
     private final ShelterDAO shelterDAO = new ShelterDAO();
 
-    /**
-     * Retrieves all shelters.
-     *
-     * @return list of shelters
-     * @throws SQLException if database access fails
-     */
     public List<Shelter> getShelters() throws SQLException {
         return shelterDAO.findAll();
     }
 
-    /**
-     * Retrieves available shelters with remaining capacity.
-     *
-     * @return list of available shelters
-     * @throws SQLException if database access fails
-     */
     public List<Shelter> getAvailableShelters() throws SQLException {
         return shelterDAO.findAvailable();
     }
 
-    /**
-     * Retrieves a shelter by ID.
-     *
-     * @param shelterId shelter identifier
-     * @return shelter if found
-     * @throws SQLException if database access fails
-     */
     public Optional<Shelter> getShelterById(int shelterId)
             throws SQLException {
         return shelterDAO.findById(shelterId);
     }
 
-    /**
-     * Updates shelter occupancy.
-     *
-     * @param shelterId shelter identifier
-     * @param occupancy current occupancy
-     * @return true if update succeeds
-     * @throws SQLException if database access fails
-     */
-    public boolean updateOccupancy(int shelterId, int occupancy)
+    public boolean updateCapacity(int shelterId, int occupancy)
             throws SQLException {
         return shelterDAO.updateOccupancy(shelterId, occupancy);
     }
